7d9211b97f47280bc5ef312efc24eb4fce2ee18e
Rafael Olmedo <rafa-olmedo@hotmail.com>
2018-07-31 21:11:47 -0300

    Add BaseEntity class with ValidationResult property.

---
+++ ReadTextFile/Entities/Base/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using ReadTextFile.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadTextFile.Entities.Base
+{
+    public class BaseEntity
+    {
+        public ValidationResult validationResults { get; set; }
+
+        public BaseEntity()
+        {
+            validationResults = new ValidationResult();
+        }
+    }
+}
